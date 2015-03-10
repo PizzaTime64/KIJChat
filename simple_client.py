e715b0317450bc6bf98b7cdd1aaf5328db98e3a3
supremasinta <supremasinta@gmail.com>
2015-03-11 00:22:20 +0700

    Update simple_client.py
    Bisa read, send, sekaligus

--- simple_client.py
+++ simple_client.py
@@ -1,12 +1,26 @@
 import socket
 import sys
+import threading
+import select
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('127.0.0.1',8888)
 client_socket.connect(server_address)
 
-recv = client_socket.recv(1024)
-print recv
-client_socket.send("hello")
-recv = client_socket.recv(1024)
-print recv
+#recv = client_socket.recv(1024)
+#print recv
+while True:
+	socket_list = [sys.stdin,client_socket]
+	readready, writeready, error = select.select(socket_list, [], [])
+
+	for sock in readready:
+		if sock == client_socket:
+			data = sock.recv(2048)
+			if not data:
+				print "Disconnected from server"
+				sys.exit()
+			else:
+				print data
+		else:
+			msg = sys.stdin.readline()
+			client_socket.send(msg)
