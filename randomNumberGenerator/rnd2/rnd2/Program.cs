f695d7eff427d6b09759c3e4e8d6b3799c1f637b
Hadrian <hadrian@hadrianbs.web.id>
2015-04-14 14:40:35 +0700

    randomnumbergenerator
    generates randum number from random directory / path name.

---
+++ randomNumberGenerator/rnd2/rnd2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+static class RandomUtil
+{
+    public static string GetRandomString()
+    {
+        string path = Path.GetRandomFileName();
+        path = path.Replace(".", ""); // Remove period.
+        return path;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        // Test the random string method.
+        Console.WriteLine(RandomUtil.GetRandomString());
+        Console.ReadLine();
+    }
+}
